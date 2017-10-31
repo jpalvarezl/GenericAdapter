b61c5e9e5e345fdea232ab85c0d1820de94de761
Jose Alvarez <jp.alvarezl@gmail.com>
2017-10-31 16:19:54 +0100

    Cleanup implementaiton further

--- app/src/main/java/com/example/jpalvarezl/abstractadapter/base/ViewHolderFactory.kt
+++ app/src/main/java/com/example/jpalvarezl/abstractadapter/base/ViewHolderFactory.kt
@@ -6,22 +6,14 @@ import android.view.ViewGroup
 class ViewHolderFactory(vararg private val binders: Binder<*, *>) {
 
     fun newViewHolder(viewType: Int, parent: ViewGroup?): BaseViewHolder<*>? {
-        var viewHolder: BaseViewHolder<*>? = null
-        for (binder in binders) {
-            if (binder.handlesViewType(viewType)) {
-                viewHolder = binder.new(LayoutInflater.from(parent!!.context).inflate(binder.viewType, parent, false))
-            }
-        }
-        return viewHolder
+        binders.filter { it.handlesViewType(viewType) }
+                .forEach { return it.new(LayoutInflater.from(parent!!.context).inflate(it.viewType, parent, false)) }
+        return null
     }
 
     fun viewTypeFromItem(item: Any): Int {
-        var viewType = -1
-        for (binder in binders) {
-            if (binder.handlesType(item)) {
-                viewType = binder.viewType
-            }
-        }
-        return viewType
+        binders.filter { it.handlesType(item) }
+                .forEach { return it.viewType }
+        return -1
     }
 }
